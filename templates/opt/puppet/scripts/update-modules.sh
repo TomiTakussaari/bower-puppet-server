1e60a00e24a368ea90e22d74ec672ef2616f5bba
Tomi Takussaari <tomi.takussaari@elisa.fi>
2015-10-12 11:39:31 +0300

    Clean lockfile if script execution fails

--- templates/opt/puppet/scripts/update-modules.sh
+++ templates/opt/puppet/scripts/update-modules.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -e
+
+function clean_up {
+	rm -f update-modules.lock
+    exit
+}
+
 if [ "$(id -u)" != "52" ]; then
    echo "This script must be run as user Puppet" 1>&2
    exit 1
@@ -10,6 +16,7 @@ pushd ${DIR} > /dev/null
 cd <%= @root_directory %>/puppet/environments
 
 lockfile -r0 update-modules.lock
+trap clean_up EXIT
 
 npm install
 npm update
@@ -17,7 +24,7 @@ npm update
 node_modules/bower/bin/bower -f install
 node_modules/bower/bin/bower -f update
 
-rm -f update-modules.lock
+clean_up
 
 popd > /dev/null
 
